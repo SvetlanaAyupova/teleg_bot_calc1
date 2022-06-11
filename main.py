7c197b848e73cb84ad980060f25a43471625f54f
SvetlanaAyupova <98208235+SvetlanaAyupova@users.noreply.github.com>
2022-06-11 15:28:41 +0500

    Update main.py
    Удалила токен

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from bot_commands import *
 
-app = ApplicationBuilder().token("5584038617:AAEQXKHLInxjd_IGY765SdHN8vgk-JDznkA").build()
+app = ApplicationBuilder().token("TOKEN").build()
 app.add_handler(CommandHandler("hi", hi_command))
 app.add_handler(CommandHandler("time", time_command))
 app.add_handler(CommandHandler("help", help_command))
